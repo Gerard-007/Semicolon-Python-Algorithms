b67a71465b8dfd24648633afc229cd2df885330f
Gerard-007 <gerardnwazk@gmail.com>
2024-09-30 14:17:08 +0100

    Done with task 6

--- phase_gate_one/python/task6.py
+++ phase_gate_one/python/task6.py
@@ -1,14 +1,21 @@
 def multiply_numbers(num1, num2):
-    total = 0
+    if isinstance(num1, int) and isinstance(num2, int):
+        total = 0
+        if num1 == 0 or num2 == 0:
+            return 0
 
-    if num1 == 0 or num2 == 0:
-        return 0
+        if num1 < 0:
+            return -multiply_numbers(-num1, num2)
+        elif num2 < 0:
+            return -multiply_numbers(num1, -num2)
+        elif num1 < 0 and num2 < 0:
+            return multiply_numbers(-num1, -num2)
 
-    if num2 < 0:
-        return -multiply_numbers(num1, -num2)
+        for _ in range(num2):
+            total += num1
+        return total
+    return "Invalid input type must be of int type"
 
-    for _ in range(num2):
-        total += num1
-    return total
-
+print(multiply_numbers(-2, -2))
 print(multiply_numbers(2, -2))
+print(multiply_numbers(-2, 2))
