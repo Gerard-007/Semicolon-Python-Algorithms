a88428ec848994fd6013deb9cef0c9d2d9c7f2dc
Gerard-007 <gerardnwazk@gmail.com>
2024-10-04 12:51:37 +0100

    Done with task_one in java and categorized each tasks by folder

---
+++ phase_gate_two/java/TaskOne.java
@@ -0,0 +1,21 @@
+public class TaskOne {
+    
+    public static void main(String[] args) {
+        checkForNegativeNumber(2244);
+    }
+
+    public static void checkForNegativeNumber(int userInput) {
+        if(userInput < 1000 || userInput > 3000) {
+            System.out.println("Invalid input must be a number between 1000 - 3000");
+        }
+        String strNumber = Integer.toString(userInput);
+        char[] charArray = strNumber.toCharArray();
+        boolean result = true;
+        for (int i = 0; i < charArray.length; i++) {
+            if(charArray[i] % 2 != 0) {
+                result = false;
+            }
+        }
+        System.out.print(result == true ? "True" : "False");
+    }
+}
