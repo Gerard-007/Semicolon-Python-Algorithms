b245d75f4a3762151b87c0f07988cd58b474c1cf
Gerard-007 <gerardnwazk@gmail.com>
2024-10-17 08:36:45 +0100

    Update algorithm to acept negative array length

--- python_tasks/16-10-2024/tap_swap.py
+++ python_tasks/16-10-2024/tap_swap.py
@@ -1,12 +1,10 @@
 def swap(array: list) -> list:
-    # if len(array) == 0 or len(array) % 2 != 0:
-    #     raise Exception("Array length must be even and non-zero.")
     result = []
-    for index in range(0, len(array), 2):
+    for index in range(0, len(array)-1 if len(array) % 2 != 0 else len(array) , 2):
         temp = array[index]
         array[index] = array[index + 1]
         array[index + 1] = temp
         result.append(array[index])
         result.append(array[index + 1])
-    
+    result.append(array[-1]) if len(array) % 2 != 0 else result
     return result
--- python_tasks/16-10-2024/test_tap_swap.py
+++ python_tasks/16-10-2024/test_tap_swap.py
@@ -7,6 +7,4 @@ class TestTapSwap(unittest.TestCase):
 
 	def test_that_tap_swap_function_returns_result(self):
 		self.assertListEqual(tap_swap.swap([1, 2, 3, 4]), [2, 1, 4, 3])
-
-	# def test_that_tap_swap_function_raises_error_with_array_length(self):
-	# 	self.assertRaises(Exception, tap_swap.swap, [1, 2, 3])
+		self.assertListEqual(tap_swap.swap([1, 2, 3, 4, 5]), [2, 1, 4, 3, 5])
